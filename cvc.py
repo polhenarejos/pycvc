c41dbc38fdd45350fafbde8e7bc9be8aabceaea1
Pol Henarejos <pol.henarejos@cttc.es>
2022-08-16 17:19:32 +0200

    Added scheme.
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

--- cvc.py
+++ cvc.py
@@ -31,8 +31,8 @@ class CVC:
     def __init__(self):
         self.__a = ASN1()
         
-    def body(self, pubkey, car, chr, role = None, valid = None, since = None, extensions = None):
-        self.__a = ASN1().add_tag(0x7f4e, self.cpi().car(car).pubkey(pubkey, car == chr).chr(chr).role(role).valid(valid, since).encode())
+    def body(self, pubkey, scheme, car, chr, role = None, valid = None, since = None, extensions = None):
+        self.__a = ASN1().add_tag(0x7f4e, self.cpi().car(car).pubkey(pubkey, scheme, car == chr).chr(chr).role(role).valid(valid, since).encode())
         return self
     
     def car(self, car):
@@ -47,19 +47,17 @@ class CVC:
         self.__a = self.__a.add_tag(0x5f29, to_bytes(val))
         return self
         
-    def pubkey(self, pubkey, full):
+    def pubkey(self, pubkey, scheme, full):
         if (isinstance(pubkey, rsa.RSAPublicKey)):
-            puboid = oid.IT_TA_ECDSA_SHA_256
             pubctx = [pubkey.public_numbers().n, pubkey.public_numbers().e]
         elif (isinstance(pubkey, ec.EllipticCurvePublicKey)):
-            puboid = oid.IT_TA_ECDSA_SHA_256
             dom = ec_domain(pubkey.public_numbers().curve)
             Y = pubkey.public_bytes(Encoding.X962, PublicFormat.UncompressedPoint)
             if (full):
                 pubctx = [dom.P, dom.A, dom.B, dom.G, dom.O, Y, dom.F]
             else:
-                pubctx = [Y]
-        self.__a = self.__a.add_object(0x7f49, puboid, pubctx)
+                pubctx = [None, None, None, None, None, Y, None]
+        self.__a = self.__a.add_object(0x7f49, scheme, pubctx)
         return self
     
     def role(self, role):
@@ -75,22 +73,37 @@ class CVC:
             self.__a = self.__a.add_tag(0x5f25, bcd(since)).add_tag(0x5f24, bcd(until))
         return self
     
-    def sign(self, key):
+    def sign(self, key, scheme):
+        
+        if (scheme == oid.ID_TA_ECDSA_SHA_1 or scheme == oid.ID_TA_RSA_PSS_SHA_1 or scheme == oid.ID_TA_RSA_V1_5_SHA_1):
+            h = hashes.SHA1()
+        elif (scheme == oid.ID_TA_ECDSA_SHA_224):
+            h = hashes.SHA224()
+        elif (scheme == oid.ID_TA_ECDSA_SHA_256 or scheme == oid.ID_TA_RSA_PSS_SHA_256 or scheme == oid.ID_TA_RSA_V1_5_SHA_256):
+            h = hashes.SHA256()
+        elif (scheme == oid.ID_TA_ECDSA_SHA_384):
+            h = hashes.SHA384()
+        elif (scheme == oid.ID_TA_ECDSA_SHA_512 or scheme == oid.ID_TA_RSA_PSS_SHA_512 or scheme == oid.ID_TA_RSA_V1_5_SHA_512):
+            h = hashes.SHA512()
         if (isinstance(key, ec.EllipticCurvePrivateKey)):
-            signature = key.sign(self.__a.encode(), ec.ECDSA(hashes.SHA256()))
+            signature = key.sign(self.__a.encode(), ec.ECDSA(h))
             r,s = utils.decode_dss_signature(signature)
             signature = to_bytes(r) + to_bytes(s)
         elif (isinstance(key, rsa.RSAPrivateKey)):
-            signature = key.sign(self.__a.buffer(), padding.PKCS1v15(), hashes.SHA256())
+            if (scheme == oid.ID_TA_RSA_V1_5_SHA_1 or scheme == oid.ID_TA_RSA_V1_5_SHA_256 or scheme == oid.ID_TA_RSA_V1_5_SHA_512):
+                p = padding.PKCS1v15()
+            elif (scheme == oid.ID_TA_RSA_PSS_SHA_1 or scheme == oid.ID_TA_RSA_PSS_SHA_256 or scheme == oid.ID_TA_RSA_PSS_SHA_512):
+                p = padding.PSS(mgf=padding.MGF1(h), salt_length=padding.PSS.MAX_LENGTH)
+            signature = key.sign(self.__a.buffer(), p, h)
         self.__a = self.__a.add_tag(0x5f37, bytearray(signature))
         return self
 
-    def cert(self, pubkey, signkey, car, chr, role, valid, since = None, extensions = None):
-        self.__a = ASN1().add_tag(0x7f21, self.body(pubkey, car, chr, role, valid, since, extensions).sign(signkey).encode())
+    def cert(self, pubkey, scheme, signkey, car, chr, role, valid, since = None, extensions = None):
+        self.__a = ASN1().add_tag(0x7f21, self.body(pubkey, scheme, car, chr, role, valid, since, extensions).sign(signkey, scheme).encode())
         return self
     
-    def req(self, pubkey, signkey, car, chr, outercar = None, outerkey = None, extensions = None):
-        cert = self.cert(pubkey, signkey, car, chr, role=None, valid=None, since=None, extensions=extensions)
+    def req(self, pubkey, scheme, signkey, car, chr, outercar = None, outerkey = None, extensions = None):
+        cert = self.cert(pubkey, scheme, signkey, car, chr, role=None, valid=None, since=None, extensions=extensions)
         if (outercar != None and outerkey != None):
             self.__a = ASN1().add_tag(0x67, cert.car(outercar).sign(outerkey).encode())
         return self
