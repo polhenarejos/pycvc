36e193113c9c924b40f5901a7e71f9fdec58738f
Pol Henarejos <pol.henarejos@cttc.es>
2023-10-07 20:41:16 +0200

    Add --print-bits flags to tests.
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

--- tests/ca-print.sh
+++ tests/ca-print.sh
@@ -4,7 +4,7 @@ mkdir -p certs
 cp ZZATCVCA00001.cvcert certs/ZZATCVCA00001
 cp ZZATDVCA00001.cvcert certs/ZZATDVCA00001
 
-cvc-print -d certs ZZATCVCA00001.cvcert
-cvc-print -d certs ZZATDVCA00001.cvcert
-cvc-print -d certs ZZATTERM00001.cvreq
-cvc-print -d certs ZZATTERM00001.cvcert
+cvc-print --print-bits -d certs ZZATCVCA00001.cvcert
+cvc-print --print-bits -d certs ZZATDVCA00001.cvcert
+cvc-print --print-bits -d certs ZZATTERM00001.cvreq
+cvc-print --print-bits -d certs ZZATTERM00001.cvcert
