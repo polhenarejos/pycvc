9e8c326152bc129304d5b70208e742041272f9a6
Pol Henarejos <pol.henarejos@cttc.es>
2023-10-08 02:46:35 +0200

    Added tests for checking certificate validity.
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

--- tests/ca-print.sh
+++ tests/ca-print.sh
@@ -1,10 +1,16 @@
 #/bin/bash -eu
 
+check_cert() {
+    e=$(cvc-print --print-bits -d certs $1)
+    grep -q "is VALID" <<< $e || (echo -e "${FAIL}" && exit 1)
+    grep -q "Certificate VALID" <<< $e || (echo -e "${FAIL}" && exit 1)
+}
+
 mkdir -p certs
 cp ZZATCVCA00001.cvcert certs/ZZATCVCA00001
 cp ZZATDVCA00001.cvcert certs/ZZATDVCA00001
 
-cvc-print --print-bits -d certs ZZATCVCA00001.cvcert
-cvc-print --print-bits -d certs ZZATDVCA00001.cvcert
-cvc-print --print-bits -d certs ZZATTERM00001.cvreq
-cvc-print --print-bits -d certs ZZATTERM00001.cvcert
+check_cert "ZZATCVCA00001.cvcert"
+check_cert "ZZATDVCA00001.cvcert"
+check_cert "ZZATTERM00001.cvreq"
+check_cert "ZZATTERM00001.cvcert"
